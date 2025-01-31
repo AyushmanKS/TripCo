6ddfd86f4b52b6dba896e150197ff005f2bb5f4e
AyushmanKS <ayushmanks1845@gmail.com>
2025-01-31 10:57:24 +0530

    prefilled myself text-field

--- lib/view/trips/controller/trip_companion_controller.dart
+++ lib/view/trips/controller/trip_companion_controller.dart
@@ -1,8 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:get/get.dart';
 
 class TripCompanionController extends GetxController {
+  final RxString userName = ''.obs;
+  final RxString userEmail = ''.obs;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseDatabase _database = FirebaseDatabase.instance;
   var selectedIndex = (-1).obs;
   var companions = <Member>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchUserName();
+    fetchUserEmail();
+  }
+
 
   void toggleSelection(int index) {
     selectedIndex.value = index;
@@ -40,6 +54,54 @@ class TripCompanionController extends GetxController {
       email: companions[index].email,
       isVerified: !companions[index].isVerified,
     );
+  }
+
+  Future<void> fetchUserName() async {
+    try {
+      User? user = _auth.currentUser;
+
+      if (user != null) {
+        String uid = user.uid;
+
+        DatabaseReference userRef = _database.ref("users/$uid/name");
+        DataSnapshot snapshot = await userRef.get();
+
+        if (snapshot.exists) {
+          userName.value = snapshot.value.toString();
+        } else {
+          userName.value = "Name not found";
+        }
+      } else {
+        userName.value = "No user logged in";
+      }
+    } catch (e) {
+      userName.value = "Error fetching name";
+      Get.snackbar("Error", "Failed to load profile: $e");
+    }
+  }
+
+  Future<void> fetchUserEmail() async {
+    try {
+      User? user = _auth.currentUser;
+
+      if (user != null) {
+        String uid = user.uid;
+
+        DatabaseReference userRef = _database.ref("users/$uid/email");
+        DataSnapshot snapshot = await userRef.get();
+
+        if (snapshot.exists) {
+          userEmail.value = snapshot.value.toString();
+        } else {
+          userEmail.value = "email not found";
+        }
+      } else {
+        userEmail.value = "No user logged in";
+      }
+    } catch (e) {
+      userEmail.value = "Error fetching email";
+      Get.snackbar("Error", "Failed to load profile: $e");
+    }
   }
 
 }
--- lib/view/trips/trip_companion_screen.dart
+++ lib/view/trips/trip_companion_screen.dart
@@ -26,12 +26,13 @@ class TripCompanionScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(5),
           child: Column(
             children: [
               companionButtons(tripCompanionController),
               Obx(() => tripCompanionController.selectedIndex.value != -1
-                  ? Expanded(child: memberInputFields(tripCompanionController, scale))
+                  ? Expanded(
+                      child: memberInputFields(tripCompanionController, scale))
                   : const SizedBox.shrink()),
             ],
           ),
@@ -40,19 +41,19 @@ class TripCompanionScreen extends StatelessWidget {
       floatingActionButton: Obx(() {
         return tripCompanionController.selectedIndex.value == 2
             ? FloatingActionButton(
-          onPressed: tripCompanionController.addMember,
-          backgroundColor: Colors.blueAccent,
-          child: const Icon(Icons.add, color: Colors.white),
-        )
+                onPressed: tripCompanionController.addMember,
+                backgroundColor: Colors.blueAccent,
+                child: const Icon(Icons.add, color: Colors.white),
+              )
             : const SizedBox.shrink();
       }),
       bottomNavigationBar: Container(
         height: scale.getScaledHeight(65),
-        padding: scale.getPadding(horizontal: 10, vertical: 10),
+        padding: scale.getPadding(horizontal: 5, vertical: 10),
         child: CustomButton(
           buttonText: 'Proceed',
           buttonColor: Colors.purple,
-          onTap: (){},
+          onTap: () {},
         ),
       ),
       resizeToAvoidBottomInset: false,
@@ -74,7 +75,7 @@ class TripCompanionScreen extends StatelessWidget {
             controller: controller,
             index: 1),
         CompanionButton(
-            label: '2+ members',
+            label: '2+ companions',
             icon: Icons.groups,
             controller: controller,
             index: 2),
@@ -82,98 +83,120 @@ class TripCompanionScreen extends StatelessWidget {
     );
   }
 
-  Widget memberInputFields(TripCompanionController controller, ScalingUtility scale) {
+  Widget memberInputFields(
+      TripCompanionController controller, ScalingUtility scale) {
     return Obx(
-          () => ListView.builder(
-        itemCount: controller.companions.length,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.symmetric(vertical: 8),
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: Colors.grey.shade100,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withValues(alpha: 0.2),
-                  blurRadius: 4,
-                  spreadRadius: 1,
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  index == 0 ? 'Myself' : 'Companion $index',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Colors.blueAccent,
+      () => Padding(
+        padding: scale.getPadding(vertical: 5),
+        child: ListView.builder(
+          itemCount: controller.companions.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: scale.getMargin(vertical: 8),
+              padding: scale.getPadding(all: 12),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withValues(alpha: 0.2),
+                    blurRadius: 4,
+                    spreadRadius: 1,
                   ),
-                ),
-                SizedBox(height: scale.getScaledHeight(8)),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        onChanged: (value) =>
-                        controller.companions[index].name = value,
-                        decoration: InputDecoration(
-                          labelText: 'Name',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                      ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    index == 0 ? 'Myself' : 'Companion $index',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.blueAccent,
                     ),
-                    SizedBox(width: scale.getScaledWidth(10)),
-                    Expanded(
-                      child: TextField(
-                        onChanged: (value) =>
-                        controller.companions[index].email = value,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: Icon(
-                            index == 0
-                                ? Icons.done_all // Always show done_all for index 0
-                                : controller.companions[index].isVerified
-                                ? Icons.done_all // For verified, show done_all
-                                : Icons.check, // For unverified, show check
-                            color: index == 0
-                                ? Colors.blue // Blue color for done_all for index 0
-                                : controller.companions[index].isVerified
-                                ? Colors.blue // Blue for verified
-                                : Colors.grey, // Grey for unverified
-                          ),
-                          onPressed: index == 0
-                              ? null // Disable onPressed for index 0
-                              : () {
-                            // Toggle the verification status for other companions
-                            controller.verifyMember(index);
+                  ),
+                  SizedBox(height: scale.getScaledHeight(8)),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: index == 0
+                              ? TextEditingController(
+                              text: tripCompanionController.userName.value)
+                              : null,
+                          onChanged: (value) {
+                            if (index != 0) {
+                              controller.companions[index].name = value;
+                            }
                           },
-                        ),
-                        if (controller.selectedIndex.value == 2 && index >= 3)
-                          IconButton(
-                            icon: const Icon(Icons.delete,
-                                color: Colors.redAccent),
-                            onPressed: () => controller.removeMember(index),
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
+                          enabled: index != 0, // Disable for index 0
+                        ),
+                      ),
+                      SizedBox(width: scale.getScaledWidth(10)),
+                      Expanded(
+                        child: TextField(
+                          controller: index == 0
+                              ? TextEditingController(
+                              text: tripCompanionController.userEmail.value)
+                              : null,
+                          onChanged: (value) {
+                            if (index != 0) {
+                              controller.companions[index].email = value;
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          enabled: index != 0, // Disable for index 0
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              index == 0
+                                  ? Icons.done_all // Always show done_all for index 0
+                                  : controller.companions[index].isVerified
+                                  ? Icons.done_all // Show done_all if verified
+                                  : Icons.check, // Show check if not verified
+                              color: index == 0
+                                  ? Colors.blue // Blue for Myself
+                                  : controller.companions[index].isVerified
+                                  ? Colors.blue // Blue if verified
+                                  : Colors.grey, // Grey if unverified
+                            ),
+                            onPressed: index == 0
+                                ? null // Disable for Myself
+                                : () {
+                              // Toggle verification for companions
+                              controller.verifyMember(index);
+                            },
+                          ),
+                          if (controller.selectedIndex.value == 2 && index >= 3)
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.redAccent),
+                              onPressed: () => controller.removeMember(index),
+                            ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              )
+,
+            );
+          },
+        ),
       ),
     );
   }
